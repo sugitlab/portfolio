0ead513fa0f0670dd58012a05b490650c1402170
sugitlab <sgmt.snj@gmail.com>
2022-03-08 22:39:49 +0900

    fix: fix the copyright year

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <div className="p-4 text-xs text-center text-gray-500 dark:text-gray-200 mx-auto">
-      <small> &copy; 2021, Sugitlab All rights reserved. </small>
+      <small> &copy; 2022, Sugitlab All rights reserved. </small>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
