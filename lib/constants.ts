1936d12f469965cb1855bf537e7e84b530e986a2
sugitlab <sgmt.snj@gmail.com>
2025-11-21 07:28:03 +0900

    feat: add seriousballoon_url constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,6 @@ export const twitter_en = "https://twitter.com/sugitdev";
 export const twitter_ja = "https://twitter.com/sugitlab";
 export const yaruhyaku_url =
   "https://apps.apple.com/ja/app/%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E3%81%93%E3%81%A8%E3%83%AA%E3%82%B9%E3%83%88%E7%AE%A1%E7%90%86%E3%82%A2%E3%83%97%E3%83%AA-%E3%82%84%E3%82%8B%E3%81%B2%E3%82%83%E3%81%8F/id1544283769";
+export const seriousballoon_url =
   "https://apps.apple.com/ja/app/seriousballoon/id1513381877";
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://sugitlab.dev';
